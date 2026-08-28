a291f6b8a790a787e6c5d112656b944489ccf0af
Heechul So <thrrkthrrk@gmail.com>
2026-08-28 23:15:34 +0900

    chore: Spring Boot 프로젝트 초기 구성

---
+++ src/main/java/io/github/abcshc/wellnessactivity/WellnessActivityServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.abcshc.wellnessactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellnessActivityServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/abcshc/wellnessactivity/WellnessActivityServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.abcshc.wellnessactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellnessActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
